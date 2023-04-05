8b60594f7cd6d36957ca281c2bea64e174149824
Evgeniy Hlamkin <98908654+GlanceFromTheScreen@users.noreply.github.com>
2023-04-05 22:54:49 +0300

    working grad 1

--- src/main.py
+++ src/main.py
@@ -1,11 +1,14 @@
 from multidimensional_problem import N_D_Problem
-from gradient_1 import gradient_method
 from Trial_Point_Method_file import trial_point_method
+from golden_egg import golden_search
 
 pr1 = N_D_Problem()
-pr1.nabla_calculate()
-print([2 * x for x in [3, 4]])
-pr1.gradient_method(0.01, [3, 4], trial_point_method)
+pr1.nabla_calculate()  # ОБЯЗАТЕЛЬНО ЗАПУСКАТЬ В НАЧАЛЕ ПРОГРАММЫ, ЕСЛИ БУДЕМ ИСПОЛЬЗОВАТЬ СИМВОЛЬНЫЕ ВЫЧИСЛЕНИЯ
+
+print('GOLDEN SEARCH:')
+pr1.gradient_method(0.1, [3, 4], golden_search)
+print('TPM:')
+pr1.gradient_method(0.1, [3, 4], trial_point_method)
 
 
 
--- src/multidimensional_problem.py
+++ src/multidimensional_problem.py
@@ -3,10 +3,13 @@ from gradient_1 import gradient_method, nabla_calculate
 
 class N_D_Problem:
     def __init__(self):
+        # число переменных в целевой функции:
         self.variables_amount = 2
-        # self.target_function_str = '2*(X[0] + X[1])**2 + 3*X[1]'
+        # строковфй формат ц.ф (нужен для символьного вычисления производной):
         self.target_function_str = '4 * X[0] + X[1] + 4 * (1 + 3 * X[0] ** 2 + X[1] ** 2) ** (1/2)'
+        #  eval - вычисляет строковое значение
         self.target_function = lambda X: eval(self.target_function_str)
+        #  вектор градиента - его элементы - символьные представления частных производных
         self.nabla_vector = []
 
     gradient_method = gradient_method
